00dc7aa3090b08ccb98ef1c50e30ff9d1a5604a5
Reinier Kaper <rp.kaper+github@gmail.com>
2022-08-29 07:52:31 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <TestComponent></TestComponent>
+    <pre>{{ c }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const a = ref(true)
+  const b = ref(false)
+
+  const c = computed(() => a.value && b.value)
+</script>
